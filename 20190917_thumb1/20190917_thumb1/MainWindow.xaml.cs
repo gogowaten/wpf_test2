43b947a9d7eefb526c854d73643d8bb0cbed1ae4
waten <gogowaten@yahoo.co.jp>
2019-09-17 23:00:51 +0900

    201909172300

--- 20190917_thumb1/20190917_thumb1/MainWindow.xaml.cs
+++ 20190917_thumb1/20190917_thumb1/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+//Thumbをマウスドラッグ移動
 
 namespace _20190917_thumb1
 {
@@ -20,9 +23,37 @@ namespace _20190917_thumb1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thumb MyThumb;
         public MainWindow()
         {
             InitializeComponent();
+
+            //Thumb作成してCanvasに追加、サイズは100x100
+            MyThumb = new Thumb
+            {
+                Width = 100,
+                Height = 100
+            };
+            MyCanvas.Children.Add(MyThumb);
+            Canvas.SetLeft(MyThumb, 200);
+            Canvas.SetTop(MyThumb, 100);
+
+            //DragDeltaイベント
+            MyThumb.DragDelta += MyThumb_DragDelta;
+
         }
+
+        //DragDeltaイベントで移動
+        private void MyThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            double yoko = e.HorizontalChange;//マウス横移動距離
+            double tate = e.VerticalChange;//マウス縦移動距離
+            double left = Canvas.GetLeft(MyThumb);//直前の位置、横
+            double top = Canvas.GetTop(MyThumb);//直前の位置、縦
+            //移動(位置指定)
+            Canvas.SetLeft(MyThumb, left + yoko);
+            Canvas.SetTop(MyThumb, top + tate);
+        }
+
     }
 }
