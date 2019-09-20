bf5e3a25f00da423cf42783486c209f0225e79d8
waten <gogowaten@yahoo.co.jp>
2019-09-21 01:14:43 +0900

    201909210114

--- 20190920_sizeChange/20190920_sizeChange/MainWindow.xaml.cs
+++ 20190920_sizeChange/20190920_sizeChange/MainWindow.xaml.cs
@@ -14,84 +14,105 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Controls.Primitives;
 
-namespace _20190920_thumb7
+
+namespace _20190920_sizeChange
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window
     {
-        private VariableThumb MyThumb;
+        VariableThumb MyThumb;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            MyThumb = new VariableThumb(MyCanvas, Colors.Pink);
-            MyThumb.Width = 100;MyThumb.Height = 120;
-            MyCanvas.Children.Add(MyThumb);
-            Canvas.SetLeft(MyThumb, 300); Canvas.SetTop(MyThumb, 200);
-            MyThumb.AddThumbs8();
-            MyThumb.Thumbs8SetLocate();
-
             MyButton1.Click += MyButton1_Click;
             MyButton2.Click += MyButton2_Click;
+
+            MyThumb = new VariableThumb(MyCanvas) { Width = 100, Height = 100 };
+            MyThumb.SetFillColor(Color.FromArgb(40, 0, 0, 0));
+            Canvas.SetLeft(MyThumb, 100); Canvas.SetTop(MyThumb, 100);
+            MyCanvas.Children.Add(MyThumb);
+
+
+            MyThumb.AddMarker();
         }
 
         private void MyButton2_Click(object sender, RoutedEventArgs e)
         {
-            MyThumb.AddThumbs8();
+            MyThumb.MarkerSize(10, 10);
         }
 
         private void MyButton1_Click(object sender, RoutedEventArgs e)
         {
-            MyThumb.RemoveThumbs8();
+            MyThumb.Width += 10;
         }
     }
 
+
     /// <summary>
-    /// //////////////////////////////////////////////////////////////////////////////
+    /// フラットなThumb、Canvasパネル以外には追加できない
+    /// マウスドラッグ移動とサイズ可変
+    /// 8つの■マーカーをマウスドラッグ移動でサイズ変更
+    /// マーカー表示は、AddMarker()を実行
+    /// 
     /// </summary>
     public class VariableThumb : FlatThumb
     {
-        //サイズ可変のThumb、8つのポイントを持つ
-        //FlatThumb MyThumb;
-        private Canvas ParentCanvas;
-        private FlatThumb[] Thumbs8;
 
-        public VariableThumb(Canvas parentCanvas, Color bgColor) : base(bgColor)
+        private Canvas ParentCanvas;
+        private FlatThumb[] Marker8;
+
+        public VariableThumb(Canvas parentCanvas, double width = 100, double height = 100)
         {
             ParentCanvas = parentCanvas;
+            this.Width = width;
+            this.Height = height;
 
-            //8つの■作成
+            //8つのマーカー■作成
             //配列の位置で配置する位置に対応            
             //┏━┓ 3,4,5
             //┃  ┃ 2   6
             //┗━┛ 1,0,7
             //順番は下から時計回りで、下、左下、左、左上、上、右上、右、右下
             //Tagに目印のindex番号を入れる
-            Thumbs8 = new FlatThumb[8];
+            Marker8 = new FlatThumb[8];
             for (int i = 0; i < 8; i++)
             {
-                //var ft = new FlatThumb(Colors.Cyan) { Width = 20, Height = 20, Tag = i };
-                var ft = new FlatThumb(Color.FromArgb(30,0,255,255)) { Width = 20, Height = 20, Tag = i };
-                Thumbs8[i] = ft;
-                ft.DragDelta += DirectionThumb_DragDelta;
-                ft.SetOutLine(Colors.Black, 1);
+                var ft = new FlatThumb() { Width = 20, Height = 20, Tag = i };
+                ft.SetFillColor(Color.FromArgb(30, 0, 255, 255));
+                Marker8[i] = ft;
+                ft.DragDelta += MarkerThumb_DragDelta;
+                //ft.SetOutLine(Colors.Black, 1);//実線の枠
+                //破線の枠
+                ft.SetOutLine(Colors.Black, 1, new DoubleCollection(new List<double> { 2, 4 }));
                 ft.Cursor = Cursors.Hand;
             }
 
             this.DragDelta += VariableThumb_DragDelta;
+            this.SizeChanged += VariableThumb_SizeChanged;
 
+            //AddThumbs8();
+            //Thumbs8SetLocate();
+        }
+
+        private void VariableThumb_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //8つのマーカーThumbの位置変更
+            MarkerSetLocate();
         }
 
         private void VariableThumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
+            //ドラッグ移動
             Canvas.SetLeft(this, Canvas.GetLeft(this) + e.HorizontalChange);
             Canvas.SetTop(this, Canvas.GetTop(this) + e.VerticalChange);
-            Thumbs8SetLocate();
+            MarkerSetLocate();
         }
 
+        //Thumbのサイズと位置からマーカーの位置を求める
         public Point[] MakeThumbs8Locate()
         {
             double left = Canvas.GetLeft(this);
@@ -108,43 +129,60 @@ namespace _20190920_thumb7
                 new Point(left + w, top),
                 new Point(left + w, top + (h / 2.0)),
                 new Point(left + w, top + h) };
-
             return points;
 
         }
+
+        //マーカーの表示位置の更新
+        public void MarkerSetLocate()
+        {
+            Point[] points = MakeThumbs8Locate();
+            for (int i = 0; i < 8; i++)
+            {
+                CanvasSetPoint(Marker8[i], points[i]);
+            }
+        }
+
         private void CanvasSetPoint(Thumb thumb, Point point)
         {
             Canvas.SetLeft(thumb, point.X);
             Canvas.SetTop(thumb, point.Y);
         }
-        public void AddThumbs8()
+
+        //マーカーをThumbに表示
+        public void AddMarker()
         {
-            if (ParentCanvas.Children.Contains(Thumbs8[0])) return ;
+            if (ParentCanvas.Children.Contains(Marker8[0])) return;
             for (int i = 0; i < 8; i++)
             {
-                ParentCanvas.Children.Add(Thumbs8[i]);
-            }
-        }
-        public void Thumbs8SetLocate()
-        {
-            Point[] points = MakeThumbs8Locate();
-            for (int i = 0; i < 8; i++)
-            {
-                CanvasSetPoint(Thumbs8[i], points[i]);
-            }
-        }
-        public void RemoveThumbs8()
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                ParentCanvas.Children.Remove(Thumbs8[i]);
+                ParentCanvas.Children.Add(Marker8[i]);
             }
         }
 
-
-        private void DirectionThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        //マーカーを非表示というか除去
+        public void RemoveMarker()
         {
-            FlatThumb flatThumb = (FlatThumb)sender;
+            for (int i = 0; i < 8; i++)
+            {
+                ParentCanvas.Children.Remove(Marker8[i]);
+            }
+        }
+
+        //マーカーのサイズ変更
+        public void MarkerSize(double width, double height)
+        {
+            foreach (var item in Marker8)
+            {
+                item.Width = width;
+                item.Height = height;
+            }
+        }
+
+        //マーカーのドラッグ移動時
+        //Thumbのサイズと位置を更新する
+        private void MarkerThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            FlatThumb MarkerThumb = (FlatThumb)sender;
             double w = this.Width;
             double h = this.Height;
             double l = Canvas.GetLeft(this);
@@ -166,7 +204,7 @@ namespace _20190920_thumb7
                     w -= xMove;
                 }
             }
-            //縦幅と位置を修正
+            //縦幅と位置を計算、1ピクセル未満にならないよう修正
             void tate()
             {
                 if (h - yMove < 1)
@@ -181,8 +219,8 @@ namespace _20190920_thumb7
                 }
             }
 
-            //自身の位置とサイズ変更する
-            switch (flatThumb.Tag)
+            //Thumbの位置とサイズ変更する
+            switch (MarkerThumb.Tag)
             {
                 case 0://下
                     h += yMove;
@@ -215,7 +253,7 @@ namespace _20190920_thumb7
                 default:
                     break;
             }
-
+            //1ピクセル未満にならないよう修正
             if (w < 0) w = 1;
             if (h < 0) h = 1;
 
@@ -225,69 +263,21 @@ namespace _20190920_thumb7
             Canvas.SetLeft(this, l);
             Canvas.SetTop(this, t);
 
-            //8つのThumbの位置変更
-            Thumbs8SetLocate();
-
         }
 
     }
 
-    //public class FlatThumb : Thumb
-    //{
-    //    private Grid MyGrid;
-    //    private Size MySize;
-    //    private Border MyBorder;
-        
-    //    public FlatThumb(Color bgColor, Size size)
-    //    {
-    //        MySize = size;
-    //        SetTemplate(bgColor);
-    //    }
-    //    private void SetTemplate(Color bgColor)
-    //    {
-    //        //template作成
-    //        ControlTemplate template = new ControlTemplate(typeof(Thumb));
-
-    //        //Templateの見た目の作成、ベースにはGrid要素を使った
-    //        //任意の名前をつけておくと、これをもとに検索して要素を取得できる
-    //        template.VisualTree = new FrameworkElementFactory(typeof(Grid), "tempGrid");
-    //        this.Template = template;
-
-    //        this.ApplyTemplate();//再構築、これで名前検索で取得できる
-
-    //        //名前検索で取得して背景色指定
-    //        MyGrid = (Grid)this.Template.FindName("tempGrid", this);
-    //        MyGrid.Background = new SolidColorBrush(bgColor);
-
-    //        //枠表示用Border
-    //        MyBorder = new Border()
-    //        {
-    //            Width = MySize.Width,
-    //            Height = MySize.Height,
-    //            Background = new SolidColorBrush(bgColor)
-    //        };
-    //        MyGrid.Children.Add(MyBorder);
 
 
-    //    }
 
-    //    //枠の色と太さ指定
-    //    public void SetOutLine(Color color, double lineWidth)
-    //    {
-    //        MyBorder.BorderThickness = new Thickness(lineWidth);
-    //        MyBorder.BorderBrush = new SolidColorBrush(color);
-    //    }
-    //}
-
+    /// <summary>
+    /// フラットなThumb、枠の表示ができる
+    /// ControlTemplateにRectangleを使っている
+    /// </summary>
     public class FlatThumb : Thumb
     {
-        
         private Rectangle MyRectangle;
-        public FlatThumb(Color bgColor)
-        {            
-            SetTemplate(bgColor);
-        }
-        private void SetTemplate(Color bgColor)
+        public FlatThumb()
         {
             //template作成
             ControlTemplate template = new ControlTemplate(typeof(Thumb));
@@ -299,24 +289,27 @@ namespace _20190920_thumb7
 
             this.ApplyTemplate();//再構築、これで名前検索で取得できる
 
-            //名前検索で取得して背景色指定
+            //名前検索で取得して塗りつぶしの色を指定
             MyRectangle = (Rectangle)this.Template.FindName("tempRectangle", this);
-            MyRectangle.Fill = new SolidColorBrush(bgColor);
+            MyRectangle.Fill = new SolidColorBrush(SystemColors.ControlColor);// Color.FromArgb(100, 0, 0, 0));
         }
 
-        //破線の枠の色と太さ指定、非表示は色をTransparentにするか幅を0
-        //実線にするにはdoublecollectionを0,1とかにすればいいけど
-        public void SetOutLine(Color color, double lineWidth)
+        /// <summary>
+        /// 枠線の表示、破線の枠の色と太さ指定、非表示は色をTransparentにするか幅を0
+        /// </summary>
+        /// <param name="color">枠の色</param>
+        /// <param name="lineWidth">枠線の太さ</param>
+        /// <param name="dashArray">枠線の設定、nullで実線、破線はnew DoubleCollection(new List＜double＞ { 2, 4 })とか</param>
+        public void SetOutLine(Color color, double lineWidth, DoubleCollection dashArray = null)
         {
-            MyRectangle.StrokeDashArray = new DoubleCollection(new List<double> { 4, 2 });
+            MyRectangle.StrokeDashArray = dashArray;
             MyRectangle.StrokeThickness = lineWidth;
             MyRectangle.Stroke = new SolidColorBrush(color);
         }
 
+        public void SetFillColor(Color color)//塗りつぶしの色
+        {
+            MyRectangle.Fill = new SolidColorBrush(color);
+        }
     }
-
-
-
-
-
 }
