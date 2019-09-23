ca0a88aa64b570c2242ebd64f55514bb349cdcfd
waten <gogowaten@yahoo.co.jp>
2019-09-23 12:34:17 +0900

    201909231234

--- 20190922_Pextrim2/20190922_Pextrim2/MainWindow.xaml.cs
+++ 20190922_Pextrim2/20190922_Pextrim2/MainWindow.xaml.cs
@@ -20,9 +20,68 @@ namespace _20190922_Pextrim2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ClipboardWatcher ClipboardWatcher;
+        private List<BitmapSource> ListBitmap;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+            CheckBox_ClipCheck.Click += CheckBox_ClipCheck_Click;
+            
+            ListBitmap = new List<BitmapSource>();
+
         }
+
+        private void ClipboardWatcher_DrawClipboard(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                BitmapSource bitmap = null;
+                int count = 1;
+                int limit = 1;
+                do
+                {
+                    try
+                    {
+                        bitmap = Clipboard.GetImage();
+                        ListBitmap.Add(bitmap);
+                        MyImage.Source = bitmap;
+                        MyCanvas.Width = bitmap.PixelWidth;
+                        MyCanvas.Height = bitmap.PixelHeight;
+                        MyListBox.Items.Add(TextBox_FileName.Text + TextBox_SerialNumber.Text);
+                        int sn = int.Parse(TextBox_SerialNumber.Text)+1;
+                        //TextBox_SerialNumber.Text = $"{sn,0:D4}";
+                        TextBox_SerialNumber.Text = $"{sn,0:0000}";
+                    }
+                    catch (Exception ex)
+                    {
+                        if (count == limit)
+                        {
+                            string str = $"{limit}回試したけど画像の取得に失敗\n{ex.Message}";
+                        }
+                    }
+                    finally { count++; }
+                } while (limit >= count && bitmap == null);
+            }
+        }
+
+        private void CheckBox_ClipCheck_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckBox_ClipCheck.IsChecked == true) { ClipboardWatcher.Start(); }
+            else ClipboardWatcher.Stop();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            ClipboardWatcher = new ClipboardWatcher(
+                new System.Windows.Interop.WindowInteropHelper(this).Handle);
+            ClipboardWatcher.DrawClipboard += ClipboardWatcher_DrawClipboard;
+        }
+
     }
+
+
 }
