9ff7677a631389a6813e4d5559953ed83408477c
waten <gogowaten@yahoo.co.jp>
2019-09-29 18:30:33 +0900

    201909291830

--- 20190924_pixtrim2/20190924_pixtrim2/MainWindow.xaml.cs
+++ 20190924_pixtrim2/20190924_pixtrim2/MainWindow.xaml.cs
@@ -45,6 +45,8 @@ namespace _20190924_pixtrim2
 
             MyTest();
 
+          
+
 
             ButtonSave.Click += ButtonSave_Click;
             ButtonPreview.Click += ButtonPreview_Click;
@@ -62,7 +64,7 @@ namespace _20190924_pixtrim2
             //ListBitmap = new List<BitmapSource>();
 
             ListMyBitmapSource = new ObservableCollection<MyBitmapAndName>();
-            ListMyBitmapSource.CollectionChanged += ListName_CollectionChanged;
+
             MyButtonRemoveSelectedImtem.Click += MyButtonRemoveSelectedImtem_Click;
             MyListBox.DataContext = ListMyBitmapSource;
 
@@ -87,46 +89,41 @@ namespace _20190924_pixtrim2
 
             //切り抜き後の拡縮
             SliderSaveScale.ValueChanged += SliderSaveScale_ValueChanged;
+
+            
+
         }
 
-
-
-        private void NearestnaverScaleUp(BitmapSource bitmap, int scale)
+        //切り抜き範囲を画像内に収める、移動させる     
+        private void TrimAreaCorrection()
         {
-            int w = bitmap.PixelWidth;
-            int h = bitmap.PixelHeight;
-            int stride = w * 4;
-            byte[] pixels = new byte[h * stride];
-            bitmap.CopyPixels(pixels, stride, 0);
-
-
-            int ww = w * scale; int hh = h * scale; int stride2 = ww * 4;
-            byte[] pixels2 = new byte[hh * stride2];
-            int p, pp;
-            for (int y = 0; y < h; y++)
+            var right = MyNumericW.MyValue2 + MyNumericX.MyValue2;
+            var bottom = MyNumericH.MyValue2 + MyNumericY.MyValue2;
+            BitmapSource bmp = (BitmapSource)MyImage.Source;
+            var imgWidht = bmp.PixelWidth;
+            var imgHeight = bmp.PixelHeight;
+            var xDiff = right - imgWidht;//範囲内ならマイナス値
+            var yDiff = bottom - imgHeight;
+            if (xDiff > 0)
             {
-                for (int x = 0; x < w; x++)
+                if (MyNumericX.MyValue - xDiff < 0)
                 {
-                    p = y * stride + x * 4;
-                    for (int yy = y * scale; yy < y * scale + scale; yy++)
-                    {
-                        for (int xx = x * scale; xx < x * scale + scale; xx++)
-                        {
-                            pp = yy * stride2 + xx * 4;
-                            pixels2[pp] = pixels[p];
-                            pixels2[pp + 1] = pixels[p + 1];
-                            pixels2[pp + 2] = pixels[p + 2];
-                            pixels2[pp + 3] = pixels[p + 3];
-                        }
-                    }
-
+                    MyNumericW.MyValue = imgWidht;
                 }
+                MyNumericX.MyValue -= xDiff;
             }
-            BitmapSource bmp = BitmapSource.Create(ww, hh, 96, 96, bitmap.Format, null, pixels2, stride2);
-            SaveImage(bmp, MakeFullPath(ListMyBitmapSource[0].Name));
+            if (yDiff > 0)
+            {
+                if (MyNumericY.MyValue - yDiff < 0)
+                {
+                    MyNumericH.MyValue = imgHeight;
+                }
+                MyNumericY.MyValue -= yDiff;
+            }
         }
 
-        private void NearestnaverScaleDown(BitmapSource bitmap, int scale)
+        //  画像の拡縮、最近傍補間法
+        private BitmapSource NearestnaverScale(BitmapSource bitmap, decimal scale)
         {
             int w = bitmap.PixelWidth;
             int h = bitmap.PixelHeight;
@@ -135,17 +132,21 @@ namespace _20190924_pixtrim2
             bitmap.CopyPixels(pixels, stride, 0);
 
 
-            int ww = w * 1 / scale;
-            int hh = h * 1 / scale;
+            int ww = (int)Math.Round(w * scale);
+            int hh = (int)Math.Round(h * scale);
             int stride2 = ww * 4;
             byte[] pixels2 = new byte[hh * stride2];
             int p, pp;
+            decimal rate = 1 / scale;
             for (int y = 0; y < hh; y++)
             {
                 for (int x = 0; x < ww; x++)
                 {
-                    pp = y * stride2 + x * 4;
-                    p = (y * scale * stride) + (x * scale * 4);
+
+                    int motoX = (int)Math.Round(x * rate);
+                    int motoY = (int)Math.Round(y * rate);
+                    p = motoY * stride + motoX * 4;
+                    pp = (y * stride2) + (x * 4);
 
                     pixels2[pp] = pixels[p];
                     pixels2[pp + 1] = pixels[p + 1];
@@ -154,10 +155,8 @@ namespace _20190924_pixtrim2
 
                 }
             }
-            BitmapSource bmp = BitmapSource.Create(ww, hh, 96, 96, bitmap.Format, null, pixels2, stride2);
-            SaveImage(bmp, MakeFullPath(ListMyBitmapSource[0].Name));
+            return BitmapSource.Create(ww, hh, 96, 96, bitmap.Format, null, pixels2, stride2);
         }
-
 
         private void SliderSaveScale_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
@@ -176,9 +175,8 @@ namespace _20190924_pixtrim2
             if (ListMyBitmapSource.Count == 0) return;
             var bs = (MyBitmapAndName)MyListBox.SelectedItem;
             BitmapSource bmp = MakeCroppedBitmap(bs.Source);
-            //window1.MyPreview.RenderTransform = new ScaleTransform(0.25, 0.25);
-            int i = (int)SliderSaveScale.Value;
-            double rate = (i < 0) ? 1.0 / (-i + 1) : i + 1;
+
+            double rate = (double)GetMagnificationScale();
             var scale = new ScaleTransform(rate, rate);
 
             window1.MyPreview.RenderTransform = scale;
@@ -192,6 +190,16 @@ namespace _20190924_pixtrim2
             //切り抜いた画像を拡縮表示は別ウィンドウに表示して、それをBitmapレンダーで保存？
             //できたけど、別ウィンドウを表示した状態visivleじゃないと画像が更新されない
             //自分で拡縮したほうがいいかも？
+        }
+        //拡大率取得
+        private decimal GetMagnificationScale()
+        {
+            //スライダーの値から生成
+            //0 = 1, 1 = 2, 2 = 3,...
+            //-1 = 1 / 2, -2 = 1 / 3,...
+            int i = (int)SliderSaveScale.Value;
+            decimal rate = (i < 0) ? 1.0m / (-i + 1) : i + 1;
+            return rate;
         }
 
 
@@ -364,6 +372,8 @@ namespace _20190924_pixtrim2
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
             if (ListMyBitmapSource.Count == 0) return;
+            //切り抜き範囲チェック
+            if (CheckCropRect() == false) { return; }
 
             //リストの画像全部を保存
             for (int i = 0; i < ListMyBitmapSource.Count; i++)
@@ -372,11 +382,23 @@ namespace _20190924_pixtrim2
             }
         }
 
+
         //画像保存処理
         private void SaveImage(MyBitmapAndName data)
         {
+            //
+            BitmapSource bmp = MakeCroppedBitmap(data.Source);
+            decimal scale = GetMagnificationScale();
+            if (scale == 1)
+            {
+                SaveImage(bmp, data.Name);
+            }
+            else
+            {
+                SaveImage(NearestnaverScale(bmp, scale), data.Name);
+            }
             //CroppedBitmapで切り抜いた画像で保存
-            SaveImage(MakeCroppedBitmap(data.Source), data.Name);
+            //SaveImage(MakeCroppedBitmap(data.Source), data.Name);
         }
 
         //Bitmapをファイルに保存
@@ -413,11 +435,14 @@ namespace _20190924_pixtrim2
         private BitmapSource MakeCroppedBitmap(BitmapSource bitmap)
         {
             //切り抜き範囲取得
-            var rect = new Int32Rect((int)MyNumericX.MyValue2,
-                (int)MyNumericY.MyValue2, (int)MyNumericW.MyValue2, (int)MyNumericH.MyValue2);
+            var rect = MakeCropRect();
             return new CroppedBitmap(bitmap, rect);
         }
-
+        private Int32Rect MakeCropRect()
+        {
+            return new Int32Rect((int)MyNumericX.MyValue2,
+                (int)MyNumericY.MyValue2, (int)MyNumericW.MyValue2, (int)MyNumericH.MyValue2);
+        }
 
         //画像ファイル形式によるEncoder取得
         private BitmapEncoder GetEncoder()
@@ -443,6 +468,41 @@ namespace _20190924_pixtrim2
             }
         }
 
+        //切り抜き範囲が画像範囲内にあるかどうかのチェック
+        private bool CheckCropRect()
+        {
+            //切り抜き範囲チェック
+            BitmapSource bitmap;
+            Int32Rect intRect = MakeCropRect();
+            Rect crop = new Rect(intRect.X, intRect.Y, intRect.Width, intRect.Height);
+            for (int i = 0; i < ListMyBitmapSource.Count; i++)
+            {
+                bitmap = ListMyBitmapSource[i].Source;
+                if (CheckCropRect(bitmap, crop) == false)
+                {
+                    string str = $"切り抜き範囲が画像範囲外なので保存できない\n" +
+                        $"{ListMyBitmapSource[i].Name.ToString()}";
+                    MyListBox.SelectedItem = MyListBox.Items[i];
+                    MyListBox.ScrollIntoView(MyListBox.Items[i]);
+                    MessageBox.Show(str);
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool CheckCropRect(BitmapSource bitmap)
+        {
+            Int32Rect intRect = MakeCropRect();
+            Rect crop = new Rect(intRect.X, intRect.Y, intRect.Width, intRect.Height);
+            return CheckCropRect(bitmap, crop);
+        }
+        private bool CheckCropRect(BitmapSource bitmap, Rect crop)
+        {
+            Rect r2 = new Rect(crop.X, crop.Y, crop.Width, crop.Height);
+            Rect r1 = new Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight);
+            return r1.Contains(r2);
+        }
+
         #endregion
 
 
@@ -463,10 +523,11 @@ namespace _20190924_pixtrim2
             //rb.Render(im);
             //SaveImage2(rb, GetFileName(ListMyBitmapSource[0]));
 
-            //var bmp = MakeCroppedBitmap(ListMyBitmapSource[0].Source);
+            var bmp = MakeCroppedBitmap(ListMyBitmapSource[0].Source);
             //NearestnaverScaleDown(bmp, 2);
+            bmp = NearestnaverScale(bmp, GetMagnificationScale());
+            SaveImage(bmp, ListMyBitmapSource[0].Name);
 
-            var ti = GetStringNowTime();
         }
 
         private void MyBinding(FrameworkElement source, DependencyProperty sourceProperty,
@@ -497,10 +558,6 @@ namespace _20190924_pixtrim2
 
         }
 
-        private void ListName_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-
-        }
 
         private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -558,7 +615,7 @@ namespace _20190924_pixtrim2
         private string GetStringNowTime()
         {
             DateTime dt = DateTime.Now;
-            string str = dt.ToString("yyyyMMdd" + "_" + "HHmmss" + "_" + dt.Millisecond.ToString());
+            string str = dt.ToString("yyyyMMdd" + "_" + "HHmmss" + "_" + dt.Millisecond.ToString("000"));
             return str;
         }
 
