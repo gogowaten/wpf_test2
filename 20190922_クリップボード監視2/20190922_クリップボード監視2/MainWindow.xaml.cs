0e7bbca0b33bdabed4f65fae29a7b6173d8eab29
waten <gogowaten@yahoo.co.jp>
2019-10-04 22:55:36 +0900

    201910042255

--- 20190922_クリップボード監視2/20190922_クリップボード監視2/MainWindow.xaml.cs
+++ 20190922_クリップボード監視2/20190922_クリップボード監視2/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ namespace _20190922_クリップボード監視2
     /// </summary>
     public partial class MainWindow : Window
     {
-        ClipboardWatcher clipboardWatcher = null;
-        
+        ClipboardWatcher clipboardWatcher = null;//クリップボード監視クラス
+        BitmapSource PastBitmap;//画像の比較用、一時記録用
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,17 +37,9 @@ namespace _20190922_クリップボード監視2
             clipboardWatcher.DrawClipboard += ClipboardWatcher_DrawClipboard;
         }
 
-        //private void ClipboardWatcher_DrawClipboard(object sender, EventArgs e)
-        //{
-        //    if (Clipboard.ContainsImage())
-        //    {
-        //        var img = new Image();
-        //        img.Source = Clipboard.GetImage();//たまにエラーになる
-        //        MyListBox.Items.Add(img);
-        //    }
-        //}
 
-     
+
+
 
         //クリップボード内容変更イベント時の処理
         private void ClipboardWatcher_DrawClipboard(object sender, EventArgs e)
@@ -54,9 +47,8 @@ namespace _20190922_クリップボード監視2
             AddImage();
         }
 
-        //画像があったらlistBoxに追加        
-        //たまにエラーになるけど繰り返すと取得できるから
-        //回数制限をつけて回している
+        //クリップボードに前回と違う画像があったらlistBoxに追加        
+        //画像取得でたまにエラーになるけど繰り返すと取得できるから、回数制限をつけて回している
         //それでも取得できなかったらエラーメッセージ表示
         private void AddImage()
         {
@@ -64,16 +56,25 @@ namespace _20190922_クリップボード監視2
             {
                 var img = new Image();
                 int count = 1;
-                int limit = 5;//取得試行回数制限、1以上指定、5あれば十分
+                int limit = 5;//取得試行回数制限、1以上指定、5あれば十分、環境によるかも
+                BitmapSource NowBitmap;
                 do
                 {
                     try
                     {
-                        img.Source = Clipboard.GetImage();//たまにエラーになる
+                        //画像取得
+                        NowBitmap = Clipboard.GetImage();//ここでたまにエラーになる
+
+                        //前回の画像と今回の画像が同じなら、なにもしないでreturn
+                        if (IsBitmapEqual(PastBitmap, NowBitmap)) return;
+                        //違う画像ならリストに追加
+                        img.Source = NowBitmap;
                         MyListBox.Items.Add(img);
+                        PastBitmap = NowBitmap;
                     }
                     catch (Exception ex)
                     {
+                        //指定回数内に画像取得できなかったらメッセージ表示
                         if (count == limit)
                         {
                             string str = $"{ex.Message}\n" +
@@ -90,15 +91,46 @@ namespace _20190922_クリップボード監視2
             }
         }
 
-        ////クリップボードに画像があったら取得
-        ////エラーが出なければこれでいい
-        //private void AddImage2()
-        //{
-        //    if (Clipboard.ContainsImage())
-        //    {
-        //        BitmapSource img = Clipboard.GetImage();//たまにエラーになる
-        //    }
-        //}
+        /// <summary>
+        /// 2つのBitmapSourceが同じ画像(すべてのピクセルの色)なのか判定する、MD5のハッシュ値を作成して比較
+        /// </summary>
+        /// <param name="bmp1"></param>
+        /// <param name="bmp2"></param>
+        /// <returns></returns>
+        private bool IsBitmapEqual(BitmapSource bmp1, BitmapSource bmp2)
+        {
+            if (bmp1 == null || bmp2 == null) return false;
+            //それぞれのハッシュ値を作成
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] h1 = md5.ComputeHash(MakeBitmapByte(bmp1));
+            byte[] h2 = md5.ComputeHash(MakeBitmapByte(bmp2));
+            md5.Clear();
+            //ハッシュ値を比較
+            return IsArrayEquals(h1, h2);
+        }
+        //2つのハッシュ値を比較
+        private bool IsArrayEquals(byte[] h1, byte[] h2)
+        {
+            for (int i = 0; i < h1.Length; i++)
+            {
+                if (h1[i] != h2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //BitmapSourceをbyte配列に変換
+        private byte[] MakeBitmapByte(BitmapSource bitmap)
+        {
+            int w = bitmap.PixelWidth;
+            int h = bitmap.PixelHeight;
+            int stride = w * bitmap.Format.BitsPerPixel / 8;
+            byte[] pixels = new byte[h * stride];
+            bitmap.CopyPixels(new Int32Rect(0, 0, w, h), pixels, stride, 0);
+            return pixels;
+        }
+
 
 
 
@@ -109,13 +141,16 @@ namespace _20190922_クリップボード監視2
             MessageBox.Show("start");
         }
 
-        //クリップボード監視中止
+        //クリップボード監視停止
         private void MyButtonStop_Click(object sender, RoutedEventArgs e)
         {
             clipboardWatcher.Stop();
             MessageBox.Show("stop");
         }
     }
+
+
+
 
 
     /// <summary>
@@ -153,13 +188,15 @@ namespace _20190922_クリップボード監視2
 
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            if (msg == WM_DRAWCLIPBOARD)
+            //更新通知が来たらイベント起動
+            if (msg == WM_DRAWCLIPBOARD)//クリップボード更新がなくても、なぜか5分ごとに更新されたよって通知が来る
             {
-                this.raiseDrawClipboard();
-                handled = true;
+                this.raiseDrawClipboard();//イベント起動
+                handled = true;//コレがよくわからん
             }
             return IntPtr.Zero;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -169,7 +206,6 @@ namespace _20190922_クリップボード監視2
             hwndSource = HwndSource.FromHwnd(handle);
             hwndSource.AddHook(WndProc);
             this.handle = handle;
-            //AddClipboardFormatListener(handle);
         }
 
         //クリップボード監視開始
