69115227726d53777535aec0a3cffa0bbeed290a
waten <gogowaten@yahoo.co.jp>
2019-10-09 19:52:44 +0900

    201910091952

---
+++ 20191009_TreeView/20191009_TreeView/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20191009_TreeView
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //TreeView━TreeViewItem
+            //              ┗TreeViewItem
+            //                  ┗TreeViewItem
+            TreeViewItem item = MakeTreeViewItem("test1");
+            MyTreeView.Items.Add(item);
+
+            TreeViewItem item2 = MakeTreeViewItem("test1-1");
+            item.Items.Add(item2);
+
+            TreeViewItem item3 = MakeTreeViewItem("test1-1-1");
+            item2.Items.Add(item3);
+
+            item.Expanded += (s, e) => { };
+
+            //これでもツリー状になるけど、項目が選択できない、選択状態にならない
+            //TreeViewItem
+            //  ┗TreeViewItem            
+            //      ┗TreeViewItem
+            //          ┗TreeViewItem
+            MyTreeViewItem.Header = "rootTreeViweItem";
+
+            TreeViewItem item4 = MakeTreeViewItem("test4");
+            MyTreeViewItem.Items.Add(item4);
+
+            TreeViewItem item5 = MakeTreeViewItem("test5");
+            item4.Items.Add(item5);
+
+            TreeViewItem item6 = MakeTreeViewItem("test6");
+            item5.Items.Add(item6);
+
+            item4.Collapsed += (s, e) => { };//閉じられたとき、test5のときも発生する
+            item4.Expanded += (s, e) => { };//展開されたとき、test5のときも発生する
+            item4.Selected += (s, e) => { };//選択されたときだけど、選択できないから発生しなかった
+        }
+
+        public TreeViewItem MakeTreeViewItem(string header)
+        {
+            var item = new TreeViewItem();
+            item.Header = header;
+            return item;
+        }
+    }
+}
---
+++ 20191009_フォルダ取得/20191009_フォルダ取得/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//TreeViewのテスト
+
+namespace _20191009_フォルダ取得
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyTreeView.ItemsSource = new List<Person>()
+            {
+                new Person("neko2")
+                {
+                    Children = new List<Person>()
+                    {
+                        new Person("neko2-1"),
+                        new Person("neko2-2")
+                        {
+                            Children=new List<Person>()
+                            {
+                                new Person("neko2-2-1")
+                            }
+                        }
+                    }
+                },
+                new Person("neko3")
+                {
+                    Children =new List<Person>()
+                    {
+                        new Person("neko3-1"),
+                        new Person("neko3-2")
+                    }
+                }
+            };
+
+
+    }
+
+
+
+
+    public sealed class Person
+    {
+        public string Name { get; set; }
+        public List<Person> Children { get; set; } = new List<Person>();
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
+//WPF4.5入門 その26 「TreeViewコントロール その2」 - かずきのBlog @hatena
+//https://blog.okazuki.jp/entry/20130409/1365479109
