b9db37fecaaca708ec4ea3c177ce262be151d368
waten <gogowaten@yahoo.co.jp>
2019-11-01 23:51:02 +0900

    201911012350

--- 20191101_エクセルからコピペ2/20191101_エクセルからコピペ2/MainWindow.xaml.cs
+++ 20191101_エクセルからコピペ2/20191101_エクセルからコピペ2/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using System.Collections.ObjectModel;
 
 namespace _20191101_エクセルからコピペ2
 {
@@ -20,9 +12,77 @@ namespace _20191101_エクセルからコピペ2
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<MyBitmap> MyBitmaps = new ObservableCollection<MyBitmap>();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = MyBitmaps;
+            MyListBox.SelectionChanged += MyListBox_SelectionChanged;
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MyBitmap item = MyListBox.SelectedItem as MyBitmap;
+            if (item != null)
+            {
+                Clipboard.Clear();
+                Clipboard.SetImage(item.BitmapSource);
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyBitmaps.Clear();
+
+            var data = Clipboard.GetDataObject();
+            var neko = data.GetFormats();
+            foreach (var item in data.GetFormats())
+            {
+                if (item != DataFormats.MetafilePicture)
+                {
+                    //var obj = data.GetData(item);
+                    var ms = data.GetData(item) as System.IO.MemoryStream;
+                    var bmp = data.GetData(item) as BitmapSource;
+                    if (ms != null)
+                    {
+                        try
+                        {
+                            MyBitmaps.Add(new MyBitmap(BitmapFrame.Create(ms), item));
+                        }
+                        catch (Exception) { }
+                    }
+                    if (bmp != null)
+                    {
+                        try
+                        {
+                            MyBitmaps.Add(new MyBitmap(bmp, item));
+                        }
+                        catch (Exception) { }
+                    }
+                    if (ms == null && bmp == null)
+                    {
+                        MyBitmaps.Add(new MyBitmap(null, item));
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
+    public class MyBitmap
+    {
+        public BitmapSource BitmapSource { get; set; }
+        public string DataFormatName { get; set; }
+
+        public MyBitmap(BitmapSource source, string name)
+        {
+            BitmapSource = source;
+            DataFormatName = name;
         }
     }
 }
