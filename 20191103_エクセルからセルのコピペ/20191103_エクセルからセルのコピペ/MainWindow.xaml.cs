17c0996786701094c1b026177f2a6408507d19af
waten <gogowaten@yahoo.co.jp>
2019-11-05 23:34:37 +0900

    201911052334

--- 20191103_エクセルからセルのコピペ/20191103_エクセルからセルのコピペ/MainWindow.xaml.cs
+++ 20191103_エクセルからセルのコピペ/20191103_エクセルからセルのコピペ/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using System.Collections.ObjectModel;
-
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace _20191103_エクセルからセルのコピペ
 {
@@ -23,7 +24,21 @@ namespace _20191103_エクセルからセルのコピペ
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<MyBitmap> MyBitmaps = new ObservableCollection<MyBitmap>();
+        private const int CF_ENHMETAFILE = 14;
+        [DllImport("user32.dll")]
+        private static extern bool OpenClipboard(IntPtr hWndNewOwner);
+        [DllImport("user32.dll")]
+        private static extern int IsClipboardFormatAvailable(int wFormat);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetClipboardData(int wFormat);
+        [DllImport("user32.dll")]
+        private static extern int CloseClipboard();
+
+
+
+
+        ObservableCollection<MyBitmapSource> MyBitmaps = new ObservableCollection<MyBitmapSource>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,56 +46,191 @@ namespace _20191103_エクセルからセルのコピペ
             MyListBox.SelectionChanged += MyListBox_SelectionChanged;
         }
 
+        //クリップボードへコピー
         private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            MyBitmap item = MyListBox.SelectedItem as MyBitmap;
+            MyBitmapSource item = MyListBox.SelectedItem as MyBitmapSource;
             if (item != null && item.BitmapSource != null)
             {
                 Clipboard.Clear();
-                Clipboard.SetImage(item.BitmapSource);//おk、これが一番いい
+                Clipboard.SetImage(item.BitmapSource);//これが一番いいけど、アルファ値が255にされてしまう
+
                 //Clipboard.SetDataObject(item.BitmapSource);//なぜかセットされない
                 //Clipboard.SetData("Bitmap", item.BitmapSource);//おk、たぶんSetImageと同じ
                 //Clipboard.SetData("BitmapSource", item.BitmapSource);//セットされない
-
                 //これも動作がおかしい、他のアプリには画像がないとされるけど、このアプリには貼り付けできるけど、それも変色している
                 //Clipboard.SetData("System.Windows.Media.Imaging.BitmapSource", item.BitmapSource);
+                
+                
+                var pixels = GetPixels(item.BitmapSource);
             }
         }
+        
 
-
+        //クリップボードの画像取得してリストに追加
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MyBitmaps.Clear();
 
             var data = Clipboard.GetDataObject();
             var neko = data.GetFormats();
-            var bmp = data.GetData("Bitmap") as BitmapSource;
-            MyBitmaps.Add(new MyBitmap(bmp, "GetData\"Bitmap\""));
-
-            bmp = data.GetData("System.Windows.Media.Imaging.BitmapSource") as BitmapSource;
-            MyBitmaps.Add(new MyBitmap(bmp, "GetData\"BitmapSource\""));
+            byte[] pixels;
+            BitmapSource bmp;
 
             bmp = Clipboard.GetImage();
-            MyBitmaps.Add(new MyBitmap(bmp, "GetImage()"));
+            MyBitmaps.Add(new MyBitmapSource(bmp, "GetImage()"));
+            pixels = GetPixels(bmp);
 
+            bmp = data.GetData("Bitmap") as BitmapSource;
+            MyBitmaps.Add(new MyBitmapSource(bmp, "GetData\"Bitmap\""));
+
+            bmp = data.GetData("System.Windows.Media.Imaging.BitmapSource") as BitmapSource;
+            MyBitmaps.Add(new MyBitmapSource(bmp, "GetData\"BitmapSource\""));
+
+            if (bmp == null) return;
+            bmp = new FormatConvertedBitmap(Clipboard.GetImage(), PixelFormats.Bgr32, null, 0);
+            MyBitmaps.Add(new MyBitmapSource(bmp, "FormatConvertedBitmapでBgr32"));
+
+            var encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(Clipboard.GetImage()));
+            using (var stream = new System.IO.MemoryStream())
+            {
+                encoder.Save(stream);
+                stream.Seek(0, System.IO.SeekOrigin.Begin);
+                var decoder = new BmpBitmapDecoder(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                bmp = decoder.Frames[0];
+            }
+            MyBitmaps.Add(new MyBitmapSource(bmp, "BmpBitmapEncoderDecoder"));
+
+            var pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(Clipboard.GetImage()));
+            using (var stream = new System.IO.MemoryStream())
+            {
+                pngEncoder.Save(stream);
+                stream.Seek(0, System.IO.SeekOrigin.Begin);
+                var decoder = new PngBitmapDecoder(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                bmp = decoder.Frames[0];
+            }
+            MyBitmaps.Add(new MyBitmapSource(bmp, "PngBitmapEncoderDecoder"));
+
+            //System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap
+        }
+
+        private byte[] GetPixels(BitmapSource bmp)
+        {
+            if (bmp == null) return null;
+            int w = bmp.PixelWidth;
+            int h = bmp.PixelHeight;
+            int stride = w * 4;
+            byte[] pixels = new byte[h * stride];
+            bmp.CopyPixels(new Int32Rect(0, 0, w, h), pixels, stride, 0);
+            return pixels;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            MyBitmaps.Clear();
+
+            var data = Clipboard.GetDataObject();
+            var neko = data.GetFormats();
+            Bitmap bmp;
+
+            bmp = data.GetData("System.Drawing.Bitmap") as Bitmap;
+            MyBitmaps.Add(new MyBitmapSource(BitmapToBitmapSource(bmp, System.Drawing.Imaging.ImageFormat.Bmp), "\"System.Drawing.Bitmap\" to Bmp"));
+            MyBitmaps.Add(new MyBitmapSource(BitmapToBitmapSource(bmp, System.Drawing.Imaging.ImageFormat.Png), "\"System.Drawing.Bitmap\" to Png"));
+
+            //途中でエラーになるか真っ黒
+            //System.Drawing.Imaging.Metafile metafile;
+            //metafile = data.GetData("System.Drawing.Imaging.Metafile") as System.Drawing.Imaging.Metafile;
+            //MyBitmaps.Add(new MyBitmapSource(MetafileToBitmapSource(metafile), "\"System.Drawing.Imaging.Metafile\""));
+        }
+
+        private BitmapSource BitmapToBitmapSource(System.Drawing.Image bmp, System.Drawing.Imaging.ImageFormat imageFormat)
+        {
+            BitmapSource source;
+            using (var stream = new System.IO.MemoryStream())
+            {
+                bmp.Save(stream, imageFormat);
+                stream.Seek(0, System.IO.SeekOrigin.Begin);
+                source = BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            }
+            return source;
+        }
+        private BitmapSource MetafileToBitmapSource(System.Drawing.Imaging.Metafile metafile, System.Drawing.Imaging.ImageFormat imageFormat)
+        {
+            BitmapSource source;
+            using (var stream = new System.IO.MemoryStream())
+            {
+                metafile.Save(stream, imageFormat);
+                stream.Seek(0, System.IO.SeekOrigin.Begin);
+                source = BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            }
+            return source;
+        }
+
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MyBitmaps.Clear();
+
+            var data = Clipboard.GetDataObject();
+            var neko = data.GetFormats();
+            System.Drawing.Imaging.Metafile metafile;
+
+            //APIを使ってメタファイル取得
+            metafile = GetEnhMetafileOnClipboard(new System.Windows.Interop.WindowInteropHelper(this).Handle);
+            //メタファイル→Bitmap→BitmapSourceに変換
+            MyBitmaps.Add(new MyBitmapSource(MetafileToBitmapSource(metafile, System.Drawing.Imaging.ImageFormat.Bmp), "metafile to bmp"));
+            MyBitmaps.Add(new MyBitmapSource(MetafileToBitmapSource(metafile, System.Drawing.Imaging.ImageFormat.Png), "metafile to png"));
+
+            ////
+            ////var image = System.Windows.Forms.Clipboard.GetImage();
+            //var formsData = System.Windows.Forms.Clipboard.GetDataObject();
+            //var image = formsData.GetData("System.Drawing.Bitmap") as Bitmap;
+            ////System.Drawing.Image image = data.GetData("System.Drawing.Bitmap") as Bitmap;
+            //MyBitmaps.Add(new MyBitmapSource(BitmapToBitmapSource(image, System.Drawing.Imaging.ImageFormat.Bmp), "bmp"));
+            //MyBitmaps.Add(new MyBitmapSource(BitmapToBitmapSource(image, System.Drawing.Imaging.ImageFormat.Png), "png"));
+
+
 
         }
+
+        public static System.Drawing.Imaging.Metafile GetEnhMetafileOnClipboard(IntPtr hWnd)
+        {
+            System.Drawing.Imaging.Metafile meta = null;
+            if (OpenClipboard(hWnd))
+            {
+                try
+                {
+                    if (IsClipboardFormatAvailable(CF_ENHMETAFILE) != 0)
+                    {
+                        IntPtr hmeta = GetClipboardData(CF_ENHMETAFILE);
+                        meta = new System.Drawing.Imaging.Metafile(hmeta, true);
+                    }
+                }
+                finally
+                {
+                    CloseClipboard();
+                }
+            }
+
+            return meta;
+        }
+
+
+
     }
 
 
 
 
-    public class MyBitmap
+    public class MyBitmapSource
     {
         public BitmapSource BitmapSource { get; set; }
         public string DataFormatName { get; set; }
         public string Dpi { get; set; }
 
-        public MyBitmap(BitmapSource source, string name)
+        public MyBitmapSource(BitmapSource source, string name)
         {
             BitmapSource = source;
             DataFormatName = name;
@@ -95,4 +245,15 @@ namespace _20191103_エクセルからセルのコピペ
 
         }
     }
+
 }
+//WPFのClipboard.GetImage() のバグ？ (ソフトウェア ) - Simple is best - Yahoo!ブログ
+//https://blogs.yahoo.co.jp/elku_simple/35320048.html
+//Microsoft Officeでコピーした図をPictureBoxに表示する - .NET Tips(VB.NET, C#...)
+//https://dobon.net/vb/dotnet/graphics/getclipboardmetafile.html
+//クリップボードのデータの形式を取得する - .NET Tips(VB.NET, C#...)
+//https://dobon.net/vb/dotnet/system/clipboardformats.html
+//クリップボードの画像を表示する、クリップボードに画像をコピーする - .NET Tips(VB.NET, C#...)
+//https://dobon.net/vb/dotnet/graphics/getclipboarddata.html
+//画像のdpi（画像解像度）を取得／設定するには？［C#、VB］ - ＠IT
+//https://www.atmarkit.co.jp/fdotnet/dotnettips/961dpiresolution/dpiresolution.html
---
+++ 20191105_最近傍補間法/20191105_最近傍補間法/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20191105_最近傍補間法
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapSource Source;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private BitmapSource NearestNeighbor(BitmapSource source, decimal scale)
+        {
+            int w = source.PixelWidth;
+            int h = source.PixelHeight;
+            int stride = w * 4;
+            byte[] pixels = new byte[h * stride];
+            source.CopyPixels(new Int32Rect(0, 0, w, h), pixels, stride, 0);
+
+            //変換後ピクセル数は四捨五入
+            int ww = (int)Math.Round(w * scale, MidpointRounding.AwayFromZero);
+            int hh = (int)Math.Round(h * scale, MidpointRounding.AwayFromZero);
+            if (ww == 0 || hh == 0)
+            {
+                return source;
+            }
+
+            int sstride = ww * 4;
+            byte[] ppixels = new byte[hh * sstride];
+
+            decimal rScale = w / (decimal)ww;//逆倍率
+            int p, pp;
+            for (int y = 0; y < hh; y++)
+            {
+                for (int x = 0; x < ww; x++)
+                {
+                    pp = y * sstride + x * 4;
+                    int ny = ((int)((decimal)(y + 0.5) * rScale)) * stride;
+                    int nx = (int)((decimal)(x + 0.5) * rScale) * 4;
+                    p = ny + nx;
+                    //p = ((int)(y * rScale) * stride) + ((int)(x * rScale) * 4);//旧式
+                    ppixels[pp] = pixels[p];
+                    ppixels[pp + 1] = pixels[p + 1];
+                    ppixels[pp + 2] = pixels[p + 2];
+                    ppixels[pp + 3] = pixels[p + 3];
+                }
+            }
+            return BitmapSource.Create(ww, hh, 96, 96, source.Format, null, ppixels, sstride);
+        }
+
+        private BitmapSource NearestNeighbor(BitmapSource source, double scale)
+        {
+            int w = source.PixelWidth;
+            int h = source.PixelHeight;
+            int stride = w * 4;
+            byte[] pixels = new byte[h * stride];
+            source.CopyPixels(new Int32Rect(0, 0, w, h), pixels, stride, 0);
+
+            //変換後ピクセル数は四捨五入
+            int ww = (int)Math.Round(w * scale, MidpointRounding.AwayFromZero);
+            int hh = (int)Math.Round(h * scale, MidpointRounding.AwayFromZero);
+            if (ww == 0 || hh == 0)
+            {
+                return source;
+            }
+
+            int sstride = ww * 4;
+            byte[] ppixels = new byte[hh * sstride];
+
+            double rScale = (double)w / ww;//逆倍率
+            int p, pp;
+            for (int y = 0; y < hh; y++)
+            {
+                for (int x = 0; x < ww; x++)
+                {
+                    pp = y * sstride + x * 4;
+                    int ny = ((int)((y + 0.5) * rScale)) * stride;
+                    int nx = (int)((x + 0.5) * rScale) * 4;
+                    p = ny + nx;
+                    //p = ((int)(y * rScale) * stride) + ((int)(x * rScale) * 4);//旧式
+                    ppixels[pp] = pixels[p];
+                    ppixels[pp + 1] = pixels[p + 1];
+                    ppixels[pp + 2] = pixels[p + 2];
+                    ppixels[pp + 3] = pixels[p + 3];
+                }
+            }
+            return BitmapSource.Create(ww, hh, 96, 96, source.Format, null, ppixels, sstride);
+        }
+
+        private void ButtonUp_Click(object sender, RoutedEventArgs e)
+        {
+            var source = (BitmapSource)MyImage.Source;
+            if (source == null) return;
+            var s = (Button)sender;
+            var scale = double.Parse(s.Tag.ToString());
+            MyImage.Source = NearestNeighbor(source, scale);
+        }
+
+        private void ButtonDown_Click_1(object sender, RoutedEventArgs e)
+        {
+            var source = (BitmapSource)MyImage.Source;
+            if (source == null) return;
+            var s = (Button)sender;
+            var scale = 1 / double.Parse(s.Tag.ToString());
+            MyImage.Source = NearestNeighbor(source, scale);
+        }
+
+        private void ButtonReset_Click(object sender, RoutedEventArgs e)
+        {
+            MyImage.Source = Source;
+            MyImage.RenderTransform = new ScaleTransform(1, 1);
+        }
+
+        private void ButtonPaste_Click(object sender, RoutedEventArgs e)
+        {
+            var bmp = Clipboard.GetImage();
+            if (bmp == null) return;
+            Source = new FormatConvertedBitmap(bmp, PixelFormats.Bgra32, null, 0);
+            MyImage.Source = Source;
+        }
+
+        private void ButtonDownScaleTransform_Click(object sender, RoutedEventArgs e)
+        {
+            var s = (Button)sender;
+            var scale = 1 / double.Parse(s.Tag.ToString());
+            MyImage.RenderTransform = new ScaleTransform(scale, scale);
+            RenderOptions.SetBitmapScalingMode(MyImage, BitmapScalingMode.NearestNeighbor);
+        }
+
+        private void ButtonCopy_Click(object sender, RoutedEventArgs e)
+        {
+            var source = MyImage.Source as BitmapSource;
+            if (source == null) return;
+            Clipboard.SetImage(source);
+        }
+    }
+}
