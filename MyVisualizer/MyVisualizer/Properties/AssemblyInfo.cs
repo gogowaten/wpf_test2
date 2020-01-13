c48032f98e5b686d052aea42f5a9bce833bfc851
waten <gogowaten@yahoo.co.jp>
2020-01-13 21:39:28 +0900

    202001132139blog

--- MyVisualizer/MyVisualizer/Properties/AssemblyInfo.cs
+++ MyVisualizer/MyVisualizer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyVisualizer")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020 gogowaten")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
